3e529aafd76e08616c28c2d1d0635e0203cf71d8
oopastpongkarn <oopastpongkarn@cca.edu>
2018-04-10 23:55:10 -0700

    Almost done.
    need to work on Sula and some description? and bio and contact lol not
    really almost donw but at least it’s not falling apart even tho it can
    still crash your computer if you leave it open for too long lol bye

--- js/index.js
+++ js/index.js
@@ -1,72 +1,112 @@
-//Oh boi it's 1:19am and the entire thing kinda broke - Mon Apr 9
-
 $(document).ready(function () {
-  $("#stickyandx").hide();
+  $("#projects-window").show();
+	
+	 
+ $("#xprojects").click(function() {
+  $("#projects-window").hide();
 
+$("#works").click(function() {
+  $("#projects-window").show();
+ 
+  
+});          });          });
 
-  $("#contacteing").click(function () {
-    $("#stickyandx").show();
+// _________________________
 
-
-    $(".xstickyeing").click(function () {
-      $("#stickyandx").hide();
-
-    });
-  });
+$(document).ready(function(){
+  $(".detailed-project-windows").hide();
 });
+    
+$(".detailed-project-windows").click(function() {
+$(this).show();
+}); 
+    
+$('.xclosemex').click(function() {
+$(this).parent().hide(".detailed-project-windows");
+    
+}); 
+
+// _________________________
+
+$(document).ready(function(){
+  $("#eingsresume_lol").hide();
+
+$("#resume,#r_in_about").click(function() {
+  $("#eingsresume_lol").show();
+  
+ $("#xresume").click(function() {
+  $("#eingsresume_lol").hide();
+  
+});          });          });
+
+// _________________________
+
+$(document).ready(function(){
+  $("#about-window").hide();
+
+$("#about").click(function() {
+  $("#about-window").show();
+  
+ $("#xabout").click(function() {
+  $("#about-window").hide();
+  
+});          });          });
+
+
+ //--------C O N T A C T content -------///
+
+
+$(document).ready(function(){
+  $("#contact-window").hide();
+
+
+   $("#contacteing").click(function() {
+  $("#contact-window").show();
+  
+  
+ $("#xcontact").click(function() {
+  $("#contact-window").hide();
+  
+});          });         }); 
 
 
 
 
 //+++++++++ STAY-LEAVE-+++++++++++//
 
-$(document).ready(function () {
+$(document).ready(function(){
+  $("#leave-stay-window").hide();
+  
+ $(".miniport").click(function() {
+  $("#leave-stay-window").show();
+  
+});          });
+
+
+$(function(){
+  
+ $("#stay").click(function() {
   $("#leave-stay-window").hide();
 
-  $(".miniport").click(function () {
-    $("#leave-stay-window").show();
-<<<<<<< Updated upstream
+  
+});          }); 
 
-  });
-});
+// VVVIIIIDDDEEEEEEOOOSSS
+//$(document).ready(function(){
+// if ($('.productDescription').is(':visible')) {
+//       player.pauseVideo();
+//      }
+//  else {
+//       player.playVideo();
+//      }
+//    }); 
 
-
-=======
-
-  });
-});
-
-
->>>>>>> Stashed changes
-$(function () {
-
-  $("#stay").click(function () {
-    $("#leave-stay-window").hide();
-
-
-  });
-});
-
-
-$(document).ready(function () {
-  $(".detailed-project-windows").hide();
-});
-
-$(".detailed-project-windows").click(function () {
-  $(this).show();
-});
-
-$('.xclosemex').click(function () {
-  $(this).parent().hide(".detailed-project-windows");
-
-});
 
 // detailed projects windows down
 // detailed projects windows down
 // detailed projects windows down
 // detailed projects windows down
 
-<<<<<<< HEAD
 //abookaboutablock
 $(document).ready(function(){
 $("#abookaboutablock").click(function() {
@@ -78,31 +118,16 @@ $("#abookaboutablock").click(function() {
 //     document.body.style.background = "#c6c6c6";
   
 });          });          });
-=======
-//GD1-book
-$(document).ready(function () {
-  $("#name").click(function () {
-    $("#name-detail").show();
-
-    $("#xname").click(function () {
-      $("#name-detail").hide();
-
-    });
-  });
-});
-<<<<<<< Updated upstream
-=======
->>>>>>> origin/master
-
 
 //bobaboba
 $(document).ready(function(){
 $("#bobaboba").click(function() {
   $("#bobaboba-detail").show();
-
+//    document.body.style.background = "#818181";
   
  $("#xbobaboba").click(function() {
   $("#bobaboba-detail").hide();
+//     document.body.style.background = "#c6c6c6";
   
 });          });          });
 
@@ -110,9 +135,11 @@ $("#bobaboba").click(function() {
 $(document).ready(function(){
 $("#chrysp").click(function() {
   $("#chrysp-detail").show();
+//    document.body.style.background = "#818181";
   
  $("#xchrysp").click(function() {
   $("#chrysp-detail").hide();
+//     document.body.style.background = "#c6c6c6";
   
 });          });          });
 
@@ -120,9 +147,11 @@ $("#chrysp").click(function() {
 $(document).ready(function(){
 $("#filled").click(function() {
   $("#filled-detail").show();
+//    document.body.style.background = "#818181";
   
  $("#xfilled").click(function() {
   $("#filled-detail").hide();
+//     document.body.style.background = "#c6c6c6";
   
 });          });          });
 
@@ -131,9 +160,11 @@ $("#filled").click(function() {
 $(document).ready(function(){
 $("#appstore").click(function() {
   $("#appstore-detail").show();
+//    document.body.style.background = "#818181";
   
  $("#xappstore").click(function() {
   $("#appstore-detail").hide();
+//     document.body.style.background = "#c6c6c6";
   
 });          });          });
 
@@ -142,9 +173,11 @@ $("#appstore").click(function() {
 $(document).ready(function(){
 $("#pong").click(function() {
   $("#pong-detail").show();
+//    document.body.style.background = "#818181";
   
  $("#xpong").click(function() {
   $("#pong-detail").hide();
+//     document.body.style.background = "#c6c6c6";
   
 });          });          });
 
@@ -153,9 +186,11 @@ $("#pong").click(function() {
 $(document).ready(function(){
 $("#l3r").click(function() {
   $("#l3r-detail").show();
+//    document.body.style.background = "#818181";
   
  $("#xl3r").click(function() {
   $("#l3r-detail").hide();
+//     document.body.style.background = "#c6c6c6";
   
 });          });          });
 
@@ -163,9 +198,11 @@ $("#l3r").click(function() {
 $(document).ready(function(){
 $("#bart").click(function() {
   $("#bart-detail").show();
+//    document.body.style.background = "#818181";
   
  $("#xbart").click(function() {
   $("#bart-detail").hide();
+//     document.body.style.background = "#c6c6c6";
   
 });          });          });
 
@@ -173,9 +210,11 @@ $("#bart").click(function() {
 $(document).ready(function(){
 $("#soundjourney").click(function() {
   $("#soundjourney-detail").show();
+//    document.body.style.background = "#818181";
   
  $("#xsoundjourney").click(function() {
   $("#soundjourney-detail").hide();
+//     document.body.style.background = "#c6c6c6";
   
 });          });          });
 
@@ -183,9 +222,11 @@ $("#soundjourney").click(function() {
 $(document).ready(function(){
 $("#sula").click(function() {
   $("#sula-detail").show();
+//    document.body.style.background = "#818181";
   
  $("#xsula").click(function() {
   $("#sula-detail").hide();
+//     document.body.style.background = "#c6c6c6";
   
 });          });          });
 
@@ -193,20 +234,23 @@ $("#sula").click(function() {
 $(document).ready(function(){
 $("#cheese").click(function() {
   $("#cheese-detail").show();
+//    document.body.style.background = "#818181";
   
  $("#xcheese").click(function() {
   $("#cheese-detail").hide();
+//     document.body.style.background = "#c6c6c6";
   
 });          });          });
->>>>>>> Stashed changes
 
 //storycenter
 $(document).ready(function(){
 $("#storycenter").click(function() {
   $("#storycenter-detail").show();
+//    document.body.style.background = "#818181";
   
  $("#xstorycenter").click(function() {
   $("#storycenter-detail").hide();
+//     document.body.style.background = "#c6c6c6";
   
 });          });          });
 
@@ -214,67 +258,77 @@ $("#storycenter").click(function() {
 $(document).ready(function(){
 $("#eee").click(function() {
   $("#eee-detail").show();
+//    document.body.style.background = "#818181";
   
  $("#xeee").click(function() {
   $("#eee-detail").hide();
+//     document.body.style.background = "#c6c6c6";
   
 });          });          });
 
 
 
+
 // this makes the popup windows draggable!!!
 // this makes the popup windows draggable!!!
 // this makes the popup windows draggable!!!
 // this makes the popup windows draggable!!!
 
-$(function () {
-  var a = 500;
+$( function() {
+	var a = 500;
 
-  //bar
-  $('#projects-window,#contact-window,.detailed-project-windows').draggable({
-    handle: '.bar'
-  });
-
-  //contain in the imaginary bounding box
-  $("#projects-window,#contact-window,.detailed-project-windows").draggable({
-    containment: [-800, 0, 1350, 850]
-  });
-
-  $("#projects-window,#eingsresume_lol,#contact-window,.detailed-project-windows").draggable({
-    start: function (event, ui) { $(this).css("z-index", a++); }
-  });
-
+//bar
+   $('#projects-window,.detailed-project-windows').draggable({handle: '.bar'
 });
 
+//contain in the imaginary bounding box
+   $( "#projects-window,.detailed-project-windows" ).draggable({
+  containment: [-800, 0, 1350, 850] });
+
+	$( "#projects-window,#statement,#contact-window,.detailed-project-windows" ).draggable({
+start: function(event, ui) { $(this).css("z-index", a++); }
+});
+
+ });
+
+ 
+///////////////////
+
+// $('.click-to-front').click(function(){
+//     var topZ = 1000;
+//     $('.click-to-front').each(function(){
+//         var thisZ = parseInt($(this).css('z-index'), 500);
+//         if (thisZ > topZ){
+//             topZ = thisZ;
+//         }
+//     });
+//     $(this).css('z-index', topZ+1);
+// });
+
+// vVIDEO PLAY WHEN HOVEERRREDD
 
 
-var figure = $("#type2-id-trick-vid").hover(hoverVideo, hideVideo);
+var figure = $("#type2-id-trick-vid").hover( hoverVideo, hideVideo );
 
-function hoverVideo(e) {
-  $('video', this).get(0).play();
+function hoverVideo(e) {  
+    $('video', this).get(0).play(); 
 }
 
 function hideVideo(e) {
-  $('video', this).get(0).pause();
+    $('video', this).get(0).pause(); 
 }
 
 
 //  d r a g     b a c k g r o u n d
 
-$(function () {
+$( function() {
 
-  $(".movethis,#about-window,#leave-stay-window,#einglogo,#about,#contacteing,#works, #statement").draggable({
+   $( ".movethis,#about-window,#leave-stay-window,#einglogo,#about,#contacteing,#works,#statement,#contact-window" ).draggable({
+         
+});  
 
-  });
-
-  //    $("#eingandnav").draggable({
-  //     handle: "#einglogo"
-  //}); 
+//    $("#eingandnav").draggable({
+//     handle: "#einglogo"
+//}); 
 
 });
-
-
-
-
-
-
