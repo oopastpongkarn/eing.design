b083794315c33a1d53967d8fb26fc138d81b2a5c
Panuruj Khambanonda <pk@panurujk.com>
2019-06-30 10:02:46 -0700

    Change favicon, always run clean build

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,7 @@ const pathPrefix = config.pathPrefix === "/" ? "" : config.pathPrefix;
 module.exports = {
   pathPrefix: config.pathPrefix,
   siteMetadata: {
-    siteUrl: config.siteUrl + pathPrefix
+    siteUrl: config.siteUrl
   },
   plugins: [
     "gatsby-plugin-react-helmet",
@@ -76,18 +76,7 @@ module.exports = {
         background_color: config.backgroundColor,
         theme_color: config.themeColor,
         display: "standalone",
-        icons: [
-          {
-            src: "/favicons/android-chrome-192x192.png",
-            sizes: "192x192",
-            type: "image/png"
-          },
-          {
-            src: "/favicons/android-chrome-512x512.png",
-            sizes: "512x512",
-            type: "image/png"
-          }
-        ]
+        icon: "static/favicon.png"
       }
     }
   ]
